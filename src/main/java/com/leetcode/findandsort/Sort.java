63d37ef447e1980cdbdd5d42a8e0f2a5a4928e4e
lliyu <1171758781@qq.com>
2019-02-14 23:22:49 +0800

    排序算法

--- src/main/java/com/leetcode/findandsort/Sort.java
+++ src/main/java/com/leetcode/findandsort/Sort.java
@@ -8,21 +8,56 @@ package com.leetcode.findandsort;
 public class Sort {
     public static void main(String[] args) {
         int[] nums =new int[]{4,1,2,3,87,9,2,1,5,7,8,6,4,3};
-        bubbleSort(nums);
+        insertSort(nums);
         for (int i:nums) {
             System.out.print(i + " ");
         }
     }
 
-    public static void bubbleSort(int[] arr){
+    //选择排序
+    public static void selectSort(int[] arr){
         for(int i=0;i<arr.length;i++){
+            //一趟之后最小的数到了下标为i的位置
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i] > arr[j]){
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                    swap(arr, i, j);
                 }
             }
         }
+    }
+
+    //插入排序
+    public static void insertSort(int[] nums){
+        int i,j;
+        for(i=1;i<nums.length;i++){
+            int temp = nums[i];
+            //将元素后移
+            for(j=i-1;j>=0&&temp<nums[j];j--){
+                nums[j+1] = nums[j];
+            }
+            nums[j+1] = temp;
+        }
+
+    }
+
+
+
+    //冒泡排序
+    public static void bubbleSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+
+
+    //交换
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
     }
 }
